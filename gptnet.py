cd2f1244c7ee049a61fd9b337d9f82ccef46ddc4
shravanambudkar <shravanambudkar@gmail.com>
2025-09-11 13:11:30 +0530

    chore: format fixes and commenting

--- gptnet.py
+++ gptnet.py
@@ -3,23 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 from dataclasses import dataclass
 import math
+from typing import List
 '''----------------------------------------------------------------------------------------'''
 
 @dataclass
-class GPTconfig:
-    token_length = 1024
-    blocks = 2
-    num_heads = 2
-    h_dim = 64
-    n_dim = 64
-    vocab_size = 50257
+class GPTconfig: # Use this as an input file to configure the architecture of the GPT model
+    context_length = 1024 #maximum context length (number of tokens) the model can handle at a time
+    blocks = 2 # number of transformer blocks to be included in the GPT
+    num_heads = 2 # number of heads for multi-headed attention
+    h_dim = 64 # the embedding dimension for each token
+    lin_dim = 64 # number of neurons to be included in the linear layer of each transformer
+    vocab_size = 50257 # number of tokens in the vocabulary (50,000 BPE merges + 256 UTF-8 byte tokens + 1 End of Text token)
     
-
-
+    
 class CausalSelfAttention(nn.Module):
-    def __init__(self, config) -> None:
+    def __init__(self, config: GPTconfig) -> None:
         super().__init__()
-        self.T = config.token_length
+        self.T = config.context_length
         self.n_heads = config.num_heads
         self.h_dim = config.h_dim
         
@@ -31,24 +31,24 @@ class CausalSelfAttention(nn.Module):
         B,T,C = x.shape
         qkv_val = self.qkv_lin(x)
         q,k,v = qkv_val.split(self.h_dim,dim=2)
-        q_reproj = q.view(B,T, self.n_heads, C // self.n_heads).transpose(1,2)
-        k_reproj = k.view(B,T, self.n_heads, C // self.n_heads).transpose(1,2)
-        v_reproj = v.view(B,T, self.n_heads, C // self.n_heads).transpose(1,2)
-        att = (q_reproj @ k_reproj.transpose(-2,-1)) * (1.0/math.sqrt(k_reproj.shape[-1]))
+        q = q.view(B,T, self.n_heads, C // self.n_heads).transpose(1,2)
+        k = k.view(B,T, self.n_heads, C // self.n_heads).transpose(1,2)
+        v = v.view(B,T, self.n_heads, C // self.n_heads).transpose(1,2)
+        att = (q @ k.transpose(-2,-1)) * (1.0/math.sqrt(k.shape[-1]))
         att = att.masked_fill(self.triller[:,:,:T,:T]==0,float('-inf'))
         att = F.softmax(att,dim=-1)
-        o = att @ v_reproj
+        o = att @ v
         o = o.transpose(1,2).contiguous().view(B,T,C)
         o = self.proj(o)
         return o
-        
-        
+
+
 class MLP(nn.Module):
-    def __init__(self, config) -> None:
+    def __init__(self, config: GPTconfig) -> None:
         super().__init__()
-        self.lin1 = nn.Linear(config.h_dim,config.n_dim)
+        self.lin1 = nn.Linear(config.h_dim,config.lin_dim)
         self.gelu = nn.GELU()
-        self.lin2 = nn.Linear(config.n_dim,config.h_dim)
+        self.lin2 = nn.Linear(config.lin_dim,config.h_dim)
         
     def forward(self,x):
         x = self.lin1(x)
@@ -57,7 +57,7 @@ class MLP(nn.Module):
         return x
 
 class Block(nn.Module):
-    def __init__(self, config) -> None:
+    def __init__(self, config: GPTconfig) -> None:
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.h_dim)
         self.attn = CausalSelfAttention(config)
@@ -68,55 +68,47 @@ class Block(nn.Module):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
-    
-class GPT2(nn.Module):
+
+
+class GPT(nn.Module):
     def __init__(self, config: GPTconfig) -> None:
         super().__init__()
         self.config = config
-        
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(self.config.vocab_size,self.config.h_dim),
-            pte = nn.Embedding(self.config.token_length,self.config.h_dim),
-            h = nn.ModuleList([Block(self.config) for _ in range(config.blocks)]),
-            ln_f = nn.LayerNorm(config.h_dim)
-        )
-        )
-        
-        self.lm_head = nn.Linear(config.h_dim, config.vocab_size, bias=False)
+        self.wte = nn.Embedding(self.config.vocab_size,self.config.h_dim) # Convert each token to the given h-dim embeddings
+        self.pte = nn.Embedding(self.config.context_length,self.config.h_dim) # Calculate Positional Embedding for each token based on the index location
+        self.transformer_blocks = nn.ModuleList([Block(self.config) for _ in range(config.blocks)]) # Creating the Transformer blocks as a list
+        self.hidden_transformer_layers = nn.Sequential(*self.transformer_blocks) # unpacking the blocks as a sequential executor
+        self.ln_forward = nn.LayerNorm(config.h_dim) # final layer noramlisation layer
+        self.lm_head = nn.Linear(config.h_dim, config.vocab_size, bias=False) # Prediction of next token from the vocab
         
     def forward(self,x):
-        x_emb = self.transformer.wte(x)
-        x_pos =  self.transformer.pte(torch.tensor([i for i in range(x.shape[1])]))
+        x_emb = self.wte(x)
+        x_pos =  self.pte(torch.tensor([i for i in range(x.shape[1])])) # Building the position embedding from the length of the tokens from shape (B,T)
         x = x_emb + x_pos
-        for b in self.transformer.h:
-            x = b(x)
-        x = self.transformer.ln_f(x)
+        x = self.hidden_transformer_layers(x)
+        x = self.ln_forward(x)
         logits = self.lm_head(x)
         return logits
     
-    def generate(self,idx):
-        logits = self.forward(idx)
-        last_token = logits[:,-1,:]
-        probs = F.softmax(last_token,dim=-1)
-        sample = torch.multinomial(probs,num_samples=1)
-        return torch.cat((idx,sample),dim=1).view(-1).tolist()
+    @torch.no_grad()
+    def generate(self,idx: torch.Tensor, max_new_gens: int = 1, temperature: float = 1.0) -> List[int]:
+        for _ in range(max_new_gens):
+            idx_s = idx if idx.shape[1] <= self.config.context_length else idx[:,:-self.config.context_length]
+            logits = self.forward(idx_s)
+            last_token = logits[:,-1,:] / temperature
+            probs = F.softmax(last_token,dim=-1) 
+            sample = torch.multinomial(probs,num_samples=1)
+            idx = torch.cat((idx,sample),dim=1)
+        return idx.view(-1).tolist()
     
-
-'''----------------------------------------------------------------------------------------'''
-
-from data_proc import BatchLoader
-import tiktoken
-tokenizer = tiktoken.encoding_for_model("gpt-2")
-
-def main():
-    loader = BatchLoader(batch_size=1,context_length=1024)
-    X, Y = loader.next_batch()
-    #initialize model
-    model = GPT2(GPTconfig())
-    # logits = model(X)
-    new_idx = model.generate(torch.tensor([[5962, 22307, 25, 198]]))
-    print(new_idx)
-    print(tokenizer.decode(new_idx))
-
-if __name__ == '__main__':
-    main()
+    def calculate_number_of_params(self, only_trainable: bool = False, non_embedding: bool = False) -> int:
+        
+        if only_trainable:
+            param_count = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        else:
+            param_count = sum(p.numel() for p in self.parameters() if p.requires_grad)
+            
+        if non_embedding:
+            param_count -= self.wte.weight.numel()
+            
+        return param_count
